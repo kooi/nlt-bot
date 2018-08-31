f348618450c0776203c29fe1768f5fce01be9257
Mark Kooijman <kooi.barlaeus@gmail.com>
2018-08-31 21:48:36 +0200

    Update motor-test.py

--- src/motor-test.py
+++ src/motor-test.py
@@ -1,11 +1,13 @@
 import machine
 
-spda = machine.Pin(5)
-dira = machine.Pin(0)
+spda = machine.Pin(5, machine.Pin.OUT)
 spda_pwm = machine.PWM(spda)
+dira = machine.Pin(0, machine.Pin.OUT)
+dira.value(1) #reverse
 spda_pwm.duty(512)
 
-spdb = machine.Pin(4)
-dirb = machine.Pin(2)
+spdb = machine.Pin(4, machine.Pin.OUT)
 spdb_pwm = machine.PWM(spdb)
+dirb = machine.Pin(2, machine.Pin.OUT)
+dirb.value(0) #forward
 spdb_pwm.duty(512)
